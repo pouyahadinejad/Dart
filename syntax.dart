bdfb8f552f27637537e3dca9901e279eeeabb0b2
pouyahadinejad <pouya.hadi1382@gmail.com>
2025-01-06 13:13:28 +0330

    create file syntax,istall

---
+++ syntax.dart
@@ -0,0 +1,27 @@
+//10
+import 'dart:io';
+void main(List<String> args) {
+  var m = 21;
+  int a = 0;
+  String b = 'pouya';
+  print(m);
+  print("Hello Dart");
+  void aa() {}
+
+  printMsg() => print('Hello');
+
+  if (1 == 1) {}
+  
+  for (int i = 0; i < 10; i++) {
+    print(i);
+  }
+  //////// run in termianl
+  print('whats your name');
+  var name = stdin.readLineSync();
+  print('How are you $name, today');
+}
+//pasvandev file => dart
+//cmd  => dart syntax.dart
+//camelCase => firstName/lastName
+
+
